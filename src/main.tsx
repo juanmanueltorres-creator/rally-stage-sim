6d03bbf74d0221005be59d3a496fe37e3e51b0ff
Juan Manuel Torres <juan.manuel.torres@mi.unc.edu.ar>
2026-08-29 21:23:15 -0300

    fix: bundle MapLibre worker through Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { setWorkerUrl } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import mapLibreWorkerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url'
 import './styles.css'
 import './spectator.css'
 import './passComparison.css'
 import { App } from './App'
+
+setWorkerUrl(mapLibreWorkerUrl)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
